2f70e650f82b62099686253fa57e1ca6c9758325
Anshul Kumar <108014798+its-Anshul-19@users.noreply.github.com>
2023-02-02 02:30:35 -0800

    Instagram

---
+++ 02-Feb/Instagram.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    int X,Y;
+	    cin>>X>>Y;
+	    if(X > (10*Y))
+	    {
+	        cout << "YES" << endl;
+	    }
+	    else {
+	        cout << "NO" <<endl;
+	    }
+	  
+	    
+	    
+	    
+	}
+	return 0;
+}
