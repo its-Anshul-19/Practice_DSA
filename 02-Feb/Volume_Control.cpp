d827f52443637fdb6395daf29bae7f18a0b23c8e
Anshul Kumar <108014798+its-Anshul-19@users.noreply.github.com>
2023-02-02 02:51:07 -0800

    Volume Control

---
+++ 02-Feb/Volume_Control.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int t;
+    cin>>t;
+    
+    while(t--){
+        
+        int x,y;
+        cin>>x>>y;
+        
+        int c;
+        c=x-y;
+        
+        cout<<abs(c)<<endl;
+        
+        
+        
+        
+        
+    }
+	return 0;
+    
+    
+    
+    
+    
+}
