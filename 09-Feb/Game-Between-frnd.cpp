9404a704504b9c240e125a0b39581a2c985d1f33
Anshul Kumar <108014798+its-Anshul-19@users.noreply.github.com>
2023-02-09 16:11:15 +0530

    Game Between Friends

---
+++ 09-Feb/Game-Between-frnd.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b,c,d;
+	    cin>>a>>b>>c>>d;
+	    
+	    if(a>=b)
+	    {
+	        b=b+c;
+	    }
+	    else
+	    {
+	        a=a+c;
+	    }
+	    if(a>=b)
+	    {
+	        b=b+d;
+	    }
+	    else
+	    {
+	        a=a+d;
+	    }
+	    if(a>=b)
+	    {
+	        cout<<'N'<<endl;
+	    }
+	    else
+	    {
+	        cout<<'S'<<endl;
+	    }
+	    
+	    
+	}
+	return 0;
+}
